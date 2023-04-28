4ab2e869b5e621df2e575733b92245a3c3e7ddf7
Buyantogtokh Bekhbayar <buynaa@Buyantogtokhs-MacBook-Pro.local>
2023-04-28 22:11:14 +0900

    Upload to GitHub

---
+++ script.js
@@ -0,0 +1,32 @@
+const menuBtn = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+
+menuBtn.addEventListener("click", () => {
+  header.classList.toggle("nav-open");
+});
+
+/* STICKY NAVIGATION */
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    // In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-96px",
+  }
+);
+obs.observe(sectionHeroEl);
